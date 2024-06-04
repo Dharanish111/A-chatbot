8c8daf42118c2491df00957adeaad7e6a0188110
Pinnamshetty Dharanish <168800443+Dharanish111@users.noreply.github.com>
2024-06-04 19:20:04 +0530

    Update AI.py

--- AI.py
+++ AI.py
@@ -15,7 +15,7 @@ def load_lottiefile(url):
     return response.json()
 
 
-lottie_animation = load_lottiefile("https://github.com/Dharanish111/FixitAI/blob/main/animation.json")
+lottie_animation = load_lottiefile("https://github.com/Dharanish111/FixitAI/main/animation.json")
 st_lottie(lottie_animation, height=300, width=400)
 
 
